61487d8cc8c88b5ce00bc0ba566ed85596bb0c55
Ilya Shegay <hello@ilyashegay.com>
2021-11-23 00:19:28 -0800

    Update tests

--- test/test.ts
+++ test/test.ts
@@ -1,94 +1,11 @@
 import Decimal from 'decimal.js'
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
+import * as I from '../src/index'
+import data from './data.json'
 
-import * as I from '../src/indicators'
-import * as T from '../src/tulind'
-import * as M from '../src/math.operators'
-import { Candle, IndicatorFnRecord } from '../src/utils'
-
-import DATA from './data.json'
-
-const indicators = {
-	ad: () => I.ad,
-	adosc: T.adosc,
-	adx: T.adx,
-	adxr: T.adxr,
-	ao: () => I.ao,
-	apo: T.apo,
-	aroon: I.aroon,
-	aroonosc: I.aroonosc,
-	atr: T.atr,
-	bbands: M.bbands,
-	bop: () => I.bop,
-	cci: I.cci,
-	cmo: T.cmo,
-	crossany: () => M.crossany,
-	crossover: () => M.crossover,
-	cvi: T.cvi,
-	decay: M.decay,
-	dema: T.dema,
-	di: T.di,
-	dm: T.dm,
-	dx: T.dx,
-	dpo: T.dpo,
-	edecay: M.edecay,
-	ema: T.ema,
-	emv: () => T.emv,
-	fisher: I.fisher,
-	fosc: M.fosc,
-	hma: M.hma,
-	kama: M.kama,
-	kvo: T.kvo,
-	linreg: M.linreg,
-	linregslope: M.linregslope,
-	linregintercept: M.linregintercept,
-	macd: T.macd,
-	marketfi: () => I.marketfi,
-	mass: T.mass,
-	max: M.max,
-	min: M.min,
-	mfi: I.mfi,
-	md: T.md,
-	medprice: () => I.medprice,
-	mom: M.mom,
-	msw: M.msw,
-	natr: T.natr,
-	nvi: () => I.nvi,
-	obv: () => I.obv,
-	ppo: T.ppo,
-	psar: I.psar,
-	pvi: () => I.pvi,
-	qstick: I.qstick,
-	roc: M.roc,
-	rocr: M.rocr,
-	rsi: T.rsi,
-	sma: M.sma,
-	stoch: T.stoch,
-	stochrsi: T.stochrsi,
-	stddev: T.stddev,
-	stderr: T.stderr,
-	tema: T.tema,
-	tr: T.tr,
-	trima: M.trima,
-	trix: T.trix,
-	tsf: M.tsf,
-	typprice: () => I.typprice,
-	var: T.variance,
-	ultosc: I.ultosc,
-	vhf: M.vhf,
-	vidya: T.vidya,
-	vosc: I.vosc,
-	volatility: T.volatility,
-	vwma: I.vwma,
-	wad: () => I.wad,
-	willr: I.willr,
-	wcprice: () => I.wcprice,
-	wilders: M.wilders,
-	wma: M.wma,
-	zlema: T.zlema,
-} as unknown as IndicatorFnRecord<
-	Candle | Decimal | Decimal[],
+const fns = { ...I.indicators, ...I.operators } as I.IndicatorFnRecord<
+	I.Candle | Decimal | Decimal[] | [Decimal, Decimal],
 	number | Decimal | Decimal[]
 >
 
@@ -114,10 +31,10 @@ function round(received: number | Decimal, expected: string) {
 	return (c ? ed : rd).toFixed()
 }
 
-const remaining = new Set(Object.keys(indicators))
+const remaining = new Set(Object.keys(fns))
 
-for (const t of DATA as Test[]) {
-	const fn = indicators[t.id]
+for (const t of data as Test[]) {
+	const fn = fns[t.id]
 	if (!fn) continue
 	remaining.delete(t.id)
 
@@ -152,10 +69,10 @@ for (const t of DATA as Test[]) {
 		const rounded = results.map((r, i) => {
 			const o = t.outputs[i]
 			if (Array.isArray(r)) {
-				if (!Array.isArray(o)) throw new Error('wrong type')
+				assert.ok(Array.isArray(o), 'output is array')
 				return r.map((n, i) => round(n, o[i]))
 			} else {
-				if (Array.isArray(o)) throw new Error('wrong type')
+				assert.ok(!Array.isArray(o), 'output is not array')
 				return round(r, o)
 			}
 		})
@@ -165,7 +82,7 @@ for (const t of DATA as Test[]) {
 }
 
 test('all indicators are tested', () => {
-	assert.is(remaining.size, 0)
+	assert.equal(Array.from(remaining), [])
 })
 
 test.run()
